b92c398aad6e583e39aead2d6cf25fb63b350ff1
Aleksander Lewicki <aleksander_lewicki@epam.com>
2020-05-03 17:17:41 +0200

    Added Lekcja6

---
+++ src/Lekcja6.java
@@ -0,0 +1,81 @@
+public class Lekcja6 {
+    public static void main(String[] args) {
+
+    }
+
+    //Print all given strings using for-each loop
+    public static void printAll(String[] strings) {
+
+    }
+
+    //Find first even number in given array int[] ints. Return 0 if there is no even numbers.
+    //example: {1,3,8,7}->8
+    public static int findFirstEven(int[] ints) {
+        return 0;
+    }
+
+    //Map int[] to boolean[]. Program should map to "true" if the number is even, otherwise should map to "false".
+    //example: {1,3,8,7}->{false, false, true, false}
+    public static boolean[] areEven(int[] ints) {
+        return null;
+    }
+
+    //Map int[] to String[] in following pattern:
+    //1 -> "one"
+    //2 -> "two"
+    //3 -> "three"
+    //4 -> "four"
+    //default -> "out of range"
+    //example: {1,3,8,7}->{"one", "three", "out of range", "out of range"}
+    public static String[] mapToString(int[] ints) {
+        return null;
+    }
+
+
+    //Program should calculate average value of all integers in given array.
+    //example: {1,3,8,7}->4.75
+    public static double calculateAverage(int[] ints) {
+        return 0;
+    }
+
+    //program should return index in array "ints" of given element "value". In case of element not found return -1.
+    //example: {1,3,7,8}, 8 -> 2
+    public static int getIndex(int[] ints, int value) {
+
+        return 0;
+    }
+
+    //Given is an sorted array "ints".
+    //Write EFFICIENT solution that will return index in array "ints" of given element "value".
+    //example: {1,3,7,8}, 8 -> 3
+    //Given is an sorted array "ints".
+    //Write EFFICIENT solution that will return index in array "ints" of given element "value".
+    //example: {1,3,7,8}, 8 -> 3
+    public static int getIndexOfSorted(int[] ints, int value) {
+        return 0;
+    }
+
+    //program should print 2d array in following way:
+    // {{"a","b"},{"c","d"}} ->
+    // a,b
+    // c,d
+    public static void print2d(String[][] strings) {
+
+    }
+
+
+    //Write a program to find the common elements between array  a and b
+    //example: {"a", "b", "c"}, {"c", "b", "d"} -> {"b", "c"}
+    public static String[] findCommon(String[] a, String[] b) {
+        return null;
+    }
+
+
+    //Given is a sorted array of unique ints
+    //Write a program to count all pairs in array.
+    //Pair is defined in following way: a+b=0
+    //example: {-7,-4,-2,-1,1,4,6,7,13} -> 2 because there are 2 pairs: -7 and 7, -4, and 4
+    public static int countPairs(int[] ints) {
+        return 0;
+    }
+}
