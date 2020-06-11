7e1b0ecfb5ffb3ccfcc4a3a86eb1160398acec75
Aleksander Lewicki <aleksander_lewicki@epam.com>
2020-06-11 19:30:36 +0200

    Added examples of Queue/Stack/Map usage

---
+++ src/collections/Person.java
@@ -0,0 +1,21 @@
+package collections;
+
+public class Person implements Comparable<Person>{
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.name.compareTo(o.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
